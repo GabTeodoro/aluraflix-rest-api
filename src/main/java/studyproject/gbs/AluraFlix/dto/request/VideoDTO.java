d3803db1a153e651ec11ef297a606e594b8406b7
Gabriel Teodoro <teodoro.gt@hotmail.com>
2022-06-12 15:23:00 -0300

    Refactoring code

--- src/main/java/studyproject/gbs/AluraFlix/dto/request/VideoDTO.java
+++ src/main/java/studyproject/gbs/AluraFlix/dto/request/VideoDTO.java
@@ -52,39 +52,6 @@ public class VideoDTO {
 //        return videosDTO;
 //    }
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public static List<VideoDTO> toVideoDTO(List<Video> videos) {
         return videos.stream().map(VideoDTO::new).collect(Collectors.toList());
     }
--- src/main/java/studyproject/gbs/AluraFlix/entity/Video.java
+++ src/main/java/studyproject/gbs/AluraFlix/entity/Video.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Video {
 
     @Id
@@ -27,42 +28,6 @@ public class Video {
 
     @Column(nullable = false)
     private String url;
-
-    public Video() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public void toVideo(VideoDTO videoDTO) {
 
